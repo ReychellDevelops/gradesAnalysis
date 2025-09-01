6b8c2f63343e9f900385dfa42cba89cc560b71d0
Reychell <yo@quieroaprender.com>
2025-08-31 23:08:03 -0500

    Se crea el archivo notes.csv qu contiene los datos de los estudiantes. Adicionalmente se realiza el comienzo del codigo main.py, en donde se realiza la importacion de las librerias os y pandas. creamos funcion principal main y funcion contarEstudiantes.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,44 @@
 # main.py - programa principal
+
+#importacion de librerias
+
+import os #libreria que permite trabajar con archivos del sistema operativo
+import pandas as pd #libreria para el analisis de datos, en este caso para el analisis de un csv
+
+#definicion de funcion para realizar conteo del total de estudiantes
+def contarEstudiantes(df):
+    #retorna el numero total de estudiantes del dataframe (tabla)
+    return len(df)
+
+#definicion de funcion principal main
+def main():
+    """
+    Punto de entrada principal del programa.
+    Se encarga de cargar el archivo CSV y mostrar las notas.
+    """
+
+    print("----------- PROYECTO DE GESTIÓN DE NOTAS -----------")
+
+    #ruta del archivo csv de manera portable
+    data_path = os.path.join("data", "notes.csv") #path es una sublibreria de os que nos ayuda a indicar bien la ruta segun el sistema operativo y join coloca bien los separadores(/ mac \\ windows)
+
+    #verificamos si el archivo existe
+    if not os.path.exists(data_path): #el .exists nos permite verificar si un archivo existe
+        print(f"⚠️ No se encontró el archivo: {data_path}")
+        return
+    
+    #cargue de datos del archivo notes.csv usando libreria pandas para que nos muestre los datos en forma de tabla
+    try: #intentaejecutar el bloque de codigo, si todo sale bien muestra la tabla con los datos
+        df = pd.read_csv(data_path, sep=";")
+        print("\n📊 Datos cargados correctamente:\n")
+        print(df)
+
+        #llamamos la funcion contarEstudiantes asignandole su retorno a la variable total
+        total = contarEstudiantes(df) #le enviamos el argmento df a la funcion para que lo pueda evaluar
+        print(f"\n👩‍🎓👨‍🎓 Número de estudiantes en el curso: {total}")
+
+    except Exception as e: #si hay algun error al leer el archivo, la excepcion se guarda en la variable e y la muestra en el print
+        print(f"❌ Error al leer el archivo CSV: {e}")
+
+if __name__ == "__main__":
+    main()
